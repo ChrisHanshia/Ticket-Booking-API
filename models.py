630911c21f7b1cfc90fcf69efa15e021a4edf636
CHRIS HANSHIA C <113849749+ChrisHanshia@users.noreply.github.com>
2023-06-14 14:39:23 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from database import Base
-from sqlalchemy import Column, Integer, String, Boolean, Date
+from sqlalchemy import Column, Integer, String, Boolean, Date, foreignKey
 
 
 class Ticket(Base):
@@ -12,6 +12,16 @@ class Ticket(Base):
     date = Column(Date)
     time = Column(String)
     boarding_station = Column(String)
+    departure_station = Column(String)
+    
+    class Train(Base):
+    __tablename__ = 'trains'
+
+    id = Column(Integer, primary_key=True)
+    train_number = Column(String, ForeignKey(Ticket.train_number))
+    train_name = Column(String)
+    train_type = Column(String)
+    starting_station = Column(String)
     departure_station = Column(String)
 
 
