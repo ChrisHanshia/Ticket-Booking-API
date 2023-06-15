ae6bbc3052a01530b2bfd27997586dcecdc8889f
CHRIS HANSHIA C <113849749+ChrisHanshia@users.noreply.github.com>
2023-06-16 00:25:00 +0530

    Update auth.py

--- router/auth.py
+++ router/auth.py
@@ -11,7 +11,7 @@ from sqlalchemy import and_
 
 router = APIRouter(
     prefix='/booking',
-    tags=['registration']
+    tags=['Ticket Booking']
 )
 
 session = SessionLocal()
@@ -39,12 +39,30 @@ class TicketRequest(BaseModel):
             raise ValueError("Invalid time format. Expected format: HH:MM")
         return value
 
+    @validator('seat_number')
+    def validate_seat_number(cls, value):
+        row_column = value.split()
+        if len(row_column) != 2:
+            raise ValueError("Invalid seat number. Seat number must have two parts separated by a space.")
+        row = row_column[0]
+        column = row_column[1]
+        valid_rows = 'ABCDEFGHIJKLMNOPQRS'
+        valid_columns = [str(i) for i in range(1, 26)]
+        if row not in valid_rows or column not in valid_columns:
+            raise ValueError(
+                "Invalid seat number. Seat number must be in the format A 1-S 25 and within the range A 1-25 to S 1-25.")
+        return value
+
 class TicketUpdate(BaseModel):
     date: date
     time: str
 
     class Config:
         orm_mode = True
+
+class SeatAvailabilityResponse(BaseModel):
+    seat_number: str
+    is_booked: bool
 
 
 def get_db():
@@ -81,7 +99,7 @@ async def book_ticket(db: db_dependency, create_user_request: TicketRequest):
     db.add(create_user_model)
     db.commit()
     db.refresh(create_user_model)
-    return {"message": "ticket booked Successfully"}
+    return {"message": "Ticket booked Successfully"}
 
 @router.get("/ticket", status_code=status.HTTP_200_OK)
 async def get_ticket_details(db: db_dependency,
@@ -95,7 +113,7 @@ def delete_ticket(ticket_id: int, db: db_dependency):
     ticket = db.query(Ticket).filter(Ticket.id == ticket_id).first()
     db.delete(ticket)
     db.commit()
-    return {'message': 'ticket cancelled Successfully'}
+    return {'message': 'Ticket cancelled Successfully'}
 
 @router.put("/tickets/{ticket_id}")
 def update_ticket(ticket_id: int, ticket_update: TicketUpdate,
@@ -107,3 +125,19 @@ def update_ticket(ticket_id: int, ticket_update: TicketUpdate,
     ticket.time = ticket_update.time
     db.commit()
     return {'message': 'Ticket updated successfully'}
+
+@router.get("/seats/availability")
+async def check_seat_availability(seat_number: str, train_number: str, date: date, db: db_dependency):
+    seat = db.query(Ticket).filter(
+        Ticket.seat_number == seat_number,
+        Ticket.train_number == train_number,
+        Ticket.date == date
+    ).first()
+    if seat:
+        if seat.is_booked:
+            return {"message": "Seat not found."}
+        else:
+            return {"message": "Seat already booked."}
+    else:
+        return {"message": "Seat is available."}
+
